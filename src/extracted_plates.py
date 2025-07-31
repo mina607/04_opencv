7becf34d32985541a247aad50f530f6df5808a3e
mina607 <choimina030607@gmail.com>
2025-07-31 13:04:50 +0900

    번호판 저장 코드로 수정

--- src/extracted_plates.py
+++ src/extracted_plates.py
@@ -2,29 +2,30 @@ import cv2
 import numpy as np
 
 win_name = "scanning"
-img = cv2.imread("../img/car3.jpg")
+img = cv2.imread("../img/car1.jpg")
 rows, cols = img.shape[:2]
 draw = img.copy()
 pts_cnt = 0
 pts = np.zeros((4,2), dtype=np.float32)
+scan_count = 0
 
-def onMouse(event, x, y, flags, param):  
-    global  pts_cnt                     
+def onMouse(event, x, y, flags, param):  #마우스 이벤트 콜백 함수 구현 ---① 
+    global  pts_cnt, scan_count                     # 마우스로 찍은 좌표의 갯수 저장
     if event == cv2.EVENT_LBUTTONDOWN:  
-        cv2.circle(draw, (x,y), 10, (0,255,0), -1) 
+        cv2.circle(draw, (x,y), 10, (0,255,0), -1) # 좌표에 초록색 동그라미 표시
         cv2.imshow(win_name, draw)
 
-        pts[pts_cnt] = [x,y]            
+        pts[pts_cnt] = [x,y]            # 마우스 좌표 저장
         pts_cnt+=1
-        if pts_cnt == 4:                      
-           
-            sm = pts.sum(axis=1)                 
-            diff = np.diff(pts, axis = 1)       
+        if pts_cnt == 4:                       # 좌표가 4개 수집됨 
+            # 좌표 4개 중 상하좌우 찾기 ---② 
+            sm = pts.sum(axis=1)                 # 4쌍의 좌표 각각 x+y 계산
+            diff = np.diff(pts, axis = 1)       # 4쌍의 좌표 각각 x-y 계산
 
-            topLeft = pts[np.argmin(sm)]         
-            bottomRight = pts[np.argmax(sm)]    
-            topRight = pts[np.argmin(diff)]     
-            bottomLeft = pts[np.argmax(diff)]   
+            topLeft = pts[np.argmin(sm)]         # x+y가 가장 값이 좌상단 좌표
+            bottomRight = pts[np.argmax(sm)]     # x+y가 가장 큰 값이 우하단 좌표
+            topRight = pts[np.argmin(diff)]     # x-y가 가장 작은 것이 우상단 좌표
+            bottomLeft = pts[np.argmax(diff)]   # x-y가 가장 큰 값이 좌하단 좌표
 
             # 변환 전 4개 좌표 
             pts1 = np.float32([topLeft, topRight, bottomRight , bottomLeft])
@@ -47,5 +48,15 @@ def onMouse(event, x, y, flags, param):
             result = cv2.warpPerspective(img, mtrx, (width, height))
             cv2.imshow('scanned', result)
 
-cv2.imwrite('scanned.jpg', result)
-print("✅ scanned.jpg 파일이 저장되었습니다.")
+            scan_count += 1
+            filename = f'scanned_{scan_count}.jpg'
+            cv2.imwrite(filename, result)
+            print(f"저장완료: {filename}")
+
+            pts_cnt = 0
+            draw[:] = img[:]
+
+cv2.imshow(win_name, img)
+cv2.setMouseCallback(win_name, onMouse)    # 마우스 콜백 함수를 GUI 윈도우에 등록 ---④
+cv2.waitKey(0)
+cv2.destroyAllWindows()
